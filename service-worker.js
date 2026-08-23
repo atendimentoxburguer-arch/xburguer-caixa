6d495415aa75ece50169a76e3ac845dbe2fb106e
atendimentoxburguer-arch <atendimentoxburguer@gmail.com>
2026-08-23 20:11:03 -0300

    Precachear módulos de segurança

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,7 @@ const PRECACHE = [
   "./caixa.html",
   "./manifest.webmanifest",
   "./pwa-register.js",
+  "./index-bootstrap.js",
 
   "./style1.css",
   "./style2.css",
@@ -56,7 +57,9 @@ const PRECACHE = [
   "./cash-conference-help.js",
   "./backup-protection.js",
   "./business-rules-integration.js",
+  "./security-hardening.js",
   "./e2e-adapter.js",
+  "./e2e-security-adapter.js",
   "./app5.js",
   "./system-guard.js",
   "./realtime.js",
